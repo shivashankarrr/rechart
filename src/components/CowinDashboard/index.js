f59a8bfa0372f3c83afd19ab76bf76ffca6f08a3
shiva <shankuu786@gmail.com>
2024-02-24 04:33:34 +0000

    second commit

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -1,8 +1,10 @@
 // Write your code here
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
 
 import VaccinationCoverage from '../VaccinationCoverage'
 import VaccinationByGender from '../VaccinationByGender'
+import VaccinationByAge from '../VaccinationByAge'
 
 import './index.css'
 
@@ -21,6 +23,7 @@ class CovinDashboard extends Component {
   }
 
   getDetails = async () => {
+    this.setState({apiStatus: apiConstants.inProgress})
     const url = 'https://apis.ccbp.in/covid-vaccination-data'
     const response = await fetch(url)
     const data = await response.json()
@@ -57,14 +60,62 @@ class CovinDashboard extends Component {
           VaccinationCoverageDetails={vaccinationDetails.last7DaysVaccination}
         />
         <VaccinationByGender
-          VaccinationByGenderDetails={vaccinationDetails.VaccinationByGender}
+          vaccinationByGenderDetails={vaccinationDetails.vaccinationByGender}
+        />
+        <VaccinationByAge
+          vaccinationByAgeDetails={vaccinationDetails.vaccineByAge}
         />
       </div>
     )
   }
 
+  renderLoadingView = () => (
+    <div data-testid="loader" className="loader">
+      <Loader type="ThreeDots" color="#ffffff" height={80} width={80} />
+    </div>
+  )
+
+  renderFailureView = () => (
+    <div className="failure-view-container">
+      <h1 className="main-heading">Something went wrong</h1>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png"
+        alt="failure view"
+        className="failure-image"
+      />
+    </div>
+  )
+
+  renderViewsBasedOnApiStatus = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiConstants.success:
+        return this.renderVaccinationStatus()
+      case apiConstants.failure:
+        return this.renderFailureView()
+      case apiConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
   render() {
-    return <div>{this.renderVaccinationStatus()}</div>
+    return (
+      <div className="main-bg-container">
+        <div className="website-logo-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+            alt="website logo"
+            className="website-logo"
+          />
+          <h1 className="website-heading">Co-WIN</h1>
+        </div>
+        <h1 className="main-heading">CoWIN Vaccination in India</h1>
+        <div>{this.renderViewsBasedOnApiStatus()}</div>
+      </div>
+    )
   }
 }
 export default CovinDashboard
--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,1 +1,39 @@
 // Write your code here
+import {PieChart, Pie, Legend, Cell} from 'recharts'
+
+import './index.css'
+
+const VaccinationByAge = props => {
+  const {vaccinationByAgeDetails} = props
+
+  return (
+    <div className="bg-container">
+      <h1 className="sub-heading">Vaccination by Age</h1>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="60%"
+          data={vaccinationByAgeDetails}
+          startAngle={360}
+          endAngle={0}
+          innerRadius="0%"
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="44-60" fill="#64c2a6" />
+          <Cell name="60 above" fill="#a3df9f" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12, fontFamily: 'Roboto'}}
+        />
+      </PieChart>
+    </div>
+  )
+}
+
+export default VaccinationByAge
